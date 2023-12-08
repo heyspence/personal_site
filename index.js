0289b83bdce15fd05004fbc8caae263948c597f1
“Spencer <Spencer@bookservo.com>
2023-12-08 10:00:59 -0500

    modal edits

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const dashDoorViewButton = document.querySelector('.view-dash-door')
 const modal = document.querySelector('.modal')
-
+const closeIcon = document.querySelector('.close-icon')
 
 const toggleIHidden = (element) => {
-    console.log(element.classList)
     if(element.classList.contains("hidden")){
         element.classList.remove("hidden")
     }else{
@@ -18,3 +17,11 @@ dashDoorViewButton.addEventListener("click", ()=>{
 modal.addEventListener("click", ()=>{
     toggleIHidden(modal)
 })
+
+closeIcon.addEventListener("click", ()=>{
+    toggleIHidden(modal)
+})
+
+document.querySelector('.modal-container').addEventListener('click', (e) => {
+    e.stopPropagation();
+});
