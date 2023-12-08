b9a77dafb295f68ddf3c25a6474ed1193bfd78d9
“Spencer <Spencer@bookservo.com>
2023-12-08 09:37:34 -0500

    basic modal

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const dashDoorViewButton = document.querySelector('.view-dash-door')
-const dashDoorIframe = document.querySelector('.dash-door-iframe')
+const modal = document.querySelector('.modal')
 
 
 const toggleIHidden = (element) => {
+    console.log(element.classList)
     if(element.classList.contains("hidden")){
         element.classList.remove("hidden")
     }else{
         element.classList.add("hidden")
     }
 }
+
 dashDoorViewButton.addEventListener("click", ()=>{
-    toggleIHidden(dashDoorIframe)
+    toggleIHidden(modal)
+})
+
+modal.addEventListener("click", ()=>{
+    toggleIHidden(modal)
 })
